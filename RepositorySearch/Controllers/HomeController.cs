c9df23603ccc76df3edc98640a4bfc3f94f1dc1d
adimadar91 <adimadar91@gmail.com>
2019-02-12 21:08:03 +0200

    commit RepositorySearch

---
+++ RepositorySearch/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        static bool isInit = false;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void addBookmark(string rep)
+        {
+            if (isInit == false)
+            {
+                isInit = true;
+                Session["bookmarks"] = new List<string>();
+            }
+
+            (Session["bookmarks"] as List<string>).Add(rep);
+
+        }
+    }
+}
